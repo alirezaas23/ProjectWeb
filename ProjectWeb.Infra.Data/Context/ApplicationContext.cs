33cc9b907b910015c4ad39012049bf299a19e967
Alireza Asgari <alirezaasgari683@gmail.com>
2022-07-17 20:56:42 +0430

    Add Project Folders And DbContext

---
+++ ProjectWeb.Infra.Data/Context/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectWeb.Infra.Data.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options) { }
+    }
+}
