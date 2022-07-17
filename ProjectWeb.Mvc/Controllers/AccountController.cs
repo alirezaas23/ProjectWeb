372800ef1275249b23e91923c2fdb8fbbbabe277
Alireza Asgari <alirezaasgari683@gmail.com>
2022-07-17 21:05:08 +0430

    Add AccountController

---
+++ ProjectWeb.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectWeb.Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
