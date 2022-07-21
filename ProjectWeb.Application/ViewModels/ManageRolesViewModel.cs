f4652f4d22f83c5772f9069b61cd14f94b8b3f7c
Alireza Asgari <alirezaasgari683@gmail.com>
2022-07-21 18:16:58 +0430

    Initial And Finish ManageRolesController

---
+++ ProjectWeb.Application/ViewModels/ManageRolesViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ProjectWeb.Application.ViewModels
+{
+    public class AddRoleViewModel
+    {
+        [Required(ErrorMessage = "لطفا نام مقام را وارد کنید")]
+        [Display(Name = "نام مقام")]
+        [MaxLength(255)]
+        [Remote("IsRoleInUse", "ManageRoles", HttpMethod = "POST", AdditionalFields = "__RequestVerificationToken")]
+        public string RoleName { get; set; }
+    }
+
+    public class ShowRoleViewModel
+    {
+        public string RoleId { get; set; }
+
+        [Required(ErrorMessage = "نام مقام جدید را وارد کنید")]
+        [MaxLength (255)]
+        [Remote("IsRoleInUse", "ManageRoles", HttpMethod = "POST", AdditionalFields = "__RequestVerificationToken")]
+        public string RoleName { get; set; }
+    }
+}
--- ProjectWeb.Mvc/Controllers/Admin/ManageRolesController.cs
+++ ProjectWeb.Mvc/Controllers/Admin/ManageRolesController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ProjectWeb.Application.ViewModels;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ProjectWeb.Mvc.Controllers.Admin
 {
@@ -26,6 +28,109 @@ namespace ProjectWeb.Mvc.Controllers.Admin
         public IActionResult AddRole()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddRole(AddRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> IsRoleInUse(string roleName)
+        {
+            var role = await _roleManager.FindByNameAsync(roleName);
+            if(role == null)
+            {
+                return Json(true);
+            }
+            else
+            {
+                return Json("این مقام از قبل وجود دارد");
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return NotFound();
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null) return NotFound();
+            var roleModel = new ShowRoleViewModel()
+            {
+                RoleId = id,
+                RoleName = role.Name,
+            };
+            return View(roleModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("DeleteRole")]
+        public async Task<IActionResult> DeleteRolePost(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return NotFound();
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null) return NotFound();
+            await _roleManager.DeleteAsync(role);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            if(string.IsNullOrEmpty(id)) return NotFound();
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null) return NotFound();
+            var roleModel = new ShowRoleViewModel()
+            {
+                RoleId = role.Id,
+                RoleName = role.Name
+            };
+
+            return View(roleModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditRole(ShowRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(model.RoleId);
+                if (role == null) return NotFound();
+                role.Name = model.RoleName;
+                var result = await _roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    return View(model);
+                }
+            }
+            return View(model);
         }
     }
 }
