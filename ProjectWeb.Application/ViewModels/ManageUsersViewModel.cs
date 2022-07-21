9a26226646893a6a4292f1c073e8f0adb5e75197
Alireza Asgari <alirezaasgari683@gmail.com>
2022-07-21 19:42:44 +0430

    Initial ManageUsersController And Add ViewModels For This Controller. Not End

---
+++ ProjectWeb.Application/ViewModels/ManageUsersViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ProjectWeb.Application.ViewModels
+{
+    public class ShowUsersViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+    }
+
+    public class ShowUserViewModel
+    {
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "لطفا نام کاربری جدید را وارد کنید")]
+        [StringLength(255, MinimumLength = 5, ErrorMessage = "نام کاربری باید حداقل 5 کاراکتر باشد")]
+        [Remote("IsUserNameInUse", "Account", HttpMethod = "POST", AdditionalFields = "__RequestVerificationToken")]
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+    }
+
+    public class AddUserToRoleViewModel
+    {
+        public AddUserToRoleViewModel()
+        {
+            UserRoles = new List<UserRolesViewModel>();
+        }
+        public string UserId { get; set; }
+        public List<UserRolesViewModel> UserRoles { get; set; }
+
+    }
+
+    public class UserRolesViewModel
+    {
+        public string RoleName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ ProjectWeb.Mvc/Controllers/Admin/ManageUsersController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectWeb.Application.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWeb.Mvc.Controllers.Admin
+{
+    [Authorize]
+    public class ManageUsersController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public ManageUsersController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.
+                Select(u => new ShowUsersViewModel()
+                {
+                    UserId = u.Id,
+                    UserEmail = u.Email,
+                    UserName = u.UserName
+                }).ToList();
+
+            return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return NotFound();
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+            var userModel = new ShowUserViewModel()
+            {
+                UserId = user.Id,
+                UserEmail = user.Email,
+                UserName = user.UserName
+            };
+
+            return View(userModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditUser(ShowUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.UserId);
+                if (user == null) return NotFound();
+                user.UserName = model.UserName;
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return NotFound();
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+            var userModel = new ShowUserViewModel()
+            {
+                UserId = user.Id,
+                UserEmail = user.Email,
+                UserName = user.UserName
+            };
+            return View(userModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUser(ShowUserViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            if (user == null) return NotFound();
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+                return View(model);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddUserToRole(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return NotFound();
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+            var roles = _roleManager.Roles.ToList();
+            var model = new AddUserToRoleViewModel()
+            {
+                UserId = user.Id
+            };
+            foreach (var role in roles)
+            {
+                if(!await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.UserRoles.Add(new UserRolesViewModel()
+                    {
+                        RoleName = role.Name.ToString()
+                    });
+                }
+            }
+            return View(model);
+        }
+    }
+}
