894c1476d5f32a497cef463e77865dc555073d92
Alireza Asgari <alirezaasgari683@gmail.com>
2022-07-21 16:30:48 +0430

    Add AdminController And ManageRolesController And Views

---
+++ ProjectWeb.Mvc/Controllers/Admin/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectWeb.Mvc.Controllers.Admin
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectWeb.Mvc/Controllers/Admin/ManageRolesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ProjectWeb.Mvc.Controllers.Admin
+{
+    [Authorize]
+    public class ManageRolesController : Controller
+    {
+        private RoleManager<IdentityRole> _roleManager;
+
+        public ManageRolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+    }
+}
