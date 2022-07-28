5a19ef380616881124e89adaee29f659e186aa77
Alireza Asgari <alirezaasgari683@gmail.com>
2022-07-28 21:41:53 +0430

    Responsive admin page nav

--- ProjectWeb.Mvc/wwwroot/js/admin.js
+++ ProjectWeb.Mvc/wwwroot/js/admin.js
@@ -1,3 +1,20 @@
 ﻿$(document).ready(function () {
     AOS.init();
+    $("#hamburger-icon img").click(function () {
+        var responsiveMenu = $("#responsive-menu");
+        responsiveMenu.show();
+        responsiveMenu.animate({
+            right: 0
+        }, 400);
+
+        $("body").append('<div class="body-container"></div>');
+        $(".body-container").click(function () {
+            responsiveMenu.animate({
+                right: "-220px"
+            }, 400, function () {
+                responsiveMenu.hide();
+            });
+            $(this).remove();
+        });
+    });
 });
