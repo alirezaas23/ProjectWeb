6bd8d4bb8dc1f545f44c0ea22ed4ccecb92dbe5f
Alireza Asgari <alirezaasgari683@gmail.com>
2022-08-07 20:36:35 +0430

    Edit edit account js codes

--- ProjectWeb.Mvc/wwwroot/js/site.js
+++ ProjectWeb.Mvc/wwwroot/js/site.js
@@ -11,21 +11,28 @@ function CheckInput() {
     let input = document.getElementById('phone-input').value;
     let result = document.getElementById('phone-error');
     try {
-        if (isNaN(input)) throw "لطفا شماره تماس معتبری وارد کنید!";
+        if (isNaN(input)) {
+            throw "لطفا شماره تماس معتبری وارد کنید!"
+            return false;
+        }
 
         if (input.length < 11) {
             throw "شماره تماس وارد شده معتبر نیست!";
+            return false;
         }
 
         if (input.length > 11) {
             throw "شماره تماس وارد شده معتبر نیست!";
+            return false;
         }
 
         else {
-            throw "";
+            document.getElementById('phone-error').innerHTML = "";
+            return true;
         }
     }
     catch (error) {
         result.innerHTML = error;
+        return false;
     }
 }
