3d42ce4b272e3c4acd68be56813ba1cc30c1c6d0
Alireza Asgari <alirezaasgari683@gmail.com>
2022-08-24 22:29:54 +0430

    Add Reponsive nav

--- ProjectWeb.Mvc/wwwroot/js/site.js
+++ ProjectWeb.Mvc/wwwroot/js/site.js
@@ -38,3 +38,28 @@ function CheckInput() {
 $("#custom-tooltip").tooltip();
 $("#logout-tooltip").tooltip();
 
+function ShowMenu() {
+    var responsiveNav = document.getElementById('responsive-nav');
+    responsiveNav.style.display = "block";
+    var pos = -220;
+    var timer = setInterval(showNav, 1);
+    function showNav() {
+        if (pos == 0) {
+            clearInterval(timer);
+        }
+        else {
+            pos += 5;
+            responsiveNav.style.right = pos + "px";
+        }
+    }
+
+    var container = document.createElement("div");
+    container.style.position = "absolute";
+    container.style.top = "0";
+    container.style.right = "0";
+    container.style.bottom = "0";
+    container.style.left = "0";
+    container.style.background = "rgba(0, 0, 0, 0.7)";
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(container);
+}
