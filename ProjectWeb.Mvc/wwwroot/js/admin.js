47c977bd30fd404ef3680b6b1fbc3c1c4d4dfbbc
Alireza Asgari <alirezaasgari683@gmail.com>
2022-08-25 18:47:10 +0430

    Responsive menu for admin page and profile layout

--- ProjectWeb.Mvc/wwwroot/js/admin.js
+++ ProjectWeb.Mvc/wwwroot/js/admin.js
@@ -1,23 +1,4 @@
-﻿$(document).ready(function () {
-    AOS.init();
-    $("#hamburger-icon img").click(function () {
-        var responsiveMenu = $("#responsive-menu");
-        responsiveMenu.show();
-        responsiveMenu.animate({
-            right: 0
-        }, 400);
-
-        $("body").append('<div class="body-container"></div>');
-        $(".body-container").click(function () {
-            responsiveMenu.animate({
-                right: "-220px"
-            }, 400, function () {
-                responsiveMenu.hide();
-            });
-            $(this).remove();
-        });
-    });
-});
+﻿AOS.init();
 
 $(".custom-tooltip").tooltip();
 $(".ticket-tooltip").tooltip();
@@ -36,4 +17,48 @@ function changeIconColor() {
 function backIconColor() {
     let icon = document.getElementById('search-icon');
     icon.style.color = "#e5edf0";
+}
+
+function ShowNav() {
+    let responsiveNav = document.getElementById('responsive-menu');
+    responsiveNav.style.display = "block";
+    let pos = -220;
+    let interval = setInterval(OpenNav, 1);
+    function OpenNav() {
+        if (pos == 0) {
+            clearInterval(interval);
+        }
+        else {
+            pos += 5;
+            responsiveNav.style.right = pos + "px";
+        }
+    }
+    let container = document.createElement("div");
+    container.style.position = "absolute";
+    container.style.top = "0";
+    container.style.right = "0";
+    container.style.bottom = "0";
+    container.style.left = "0";
+    container.style.background = "rgba(0, 0, 0, 0.4)";
+    container.style.height = "100%";
+    let body = document.getElementsByTagName("body")[0];
+    body.appendChild(container);
+    container.addEventListener("click", function () {
+        this.remove();
+        let pos = 0;
+        let interval = setInterval(CloseNav, 1);
+        function CloseNav() {
+            if (pos == -220) {
+                clearInterval(interval);
+            }
+            else {
+                pos -= 5;
+                responsiveNav.style.right = pos + "px";
+            }
+        }
+    });
+    window.addEventListener("resize", function () {
+        responsiveNav.style.display = "none";
+        container.style.display = "none";
+    });
 }
