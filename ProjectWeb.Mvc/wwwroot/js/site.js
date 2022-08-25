90f9b53e302c1586fe1d0f593af10b6973a83479
Alireza Asgari <alirezaasgari683@gmail.com>
2022-08-25 18:16:38 +0430

    Create Responsive nav

--- ProjectWeb.Mvc/wwwroot/js/site.js
+++ ProjectWeb.Mvc/wwwroot/js/site.js
@@ -38,28 +38,48 @@ function CheckInput() {
 $("#custom-tooltip").tooltip();
 $("#logout-tooltip").tooltip();
 
-function ShowMenu() {
-    var responsiveNav = document.getElementById('responsive-nav');
+
+// Responsive Nav
+function ShowNav() {
+    let responsiveNav = document.getElementById('responsive-nav');
     responsiveNav.style.display = "block";
-    var pos = -220;
-    var timer = setInterval(showNav, 1);
-    function showNav() {
+    let pos = -220;
+    let interval = setInterval(OpenNav, 1);
+    function OpenNav() {
         if (pos == 0) {
-            clearInterval(timer);
+            clearInterval(interval);
         }
         else {
             pos += 5;
             responsiveNav.style.right = pos + "px";
         }
     }
-
-    var container = document.createElement("div");
-    container.style.position = "absolute";
-    container.style.top = "0";
-    container.style.right = "0";
-    container.style.bottom = "0";
-    container.style.left = "0";
-    container.style.background = "rgba(0, 0, 0, 0.7)";
-    var body = document.getElementsByTagName("body")[0];
-    body.appendChild(container);
+    let bgOverlay = document.createElement("div");
+    bgOverlay.style.position = "absolute";
+    bgOverlay.style.top = "0";
+    bgOverlay.style.right = "0";
+    bgOverlay.style.bottom = "0";
+    bgOverlay.style.left = "0";
+    bgOverlay.style.height = "100%";
+    bgOverlay.style.background = "rgba(0, 0, 0, 0.4)";
+    let body = document.getElementsByTagName("body")[0];
+    body.appendChild(bgOverlay);
+    bgOverlay.addEventListener("click", function () {
+        this.remove();
+        let pos = 0;
+        let interval = setInterval(CloseNav, 1);
+        function CloseNav() {
+            if (pos == -220) {
+                clearInterval(interval);
+            }
+            else {
+                pos -= 5;
+                responsiveNav.style.right = pos + "px";
+            }
+        }
+    });
+    window.addEventListener("resize", function () {
+        responsiveNav.style.display = "none";
+        bgOverlay.style.display = "none";
+    })
 }
