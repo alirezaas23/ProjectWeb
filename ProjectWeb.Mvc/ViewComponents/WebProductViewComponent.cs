d4afc75080ce0ce962e7eed2f5d1b25b6ccc4965
Alireza Asgari <alirezaasgari683@gmail.com>
2022-08-27 15:24:57 +0430

    Add product view components

---
+++ ProjectWeb.Mvc/ViewComponents/WebProductViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectWeb.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWeb.Mvc.ViewComponents
+{
+    public class WebProductViewComponent : ViewComponent
+    {
+        private readonly IWebProductRepository _webProductRepository;
+
+        public WebProductViewComponent(IWebProductRepository webProductRepository)
+        {
+            _webProductRepository = webProductRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var product = _webProductRepository.WebProductsList();
+            return View(product);
+        }
+    }
+}
