c9e8c55ea5d29e02af6ad8ec0da4dff7bf37f8d9
Alireza Asgari <alirezaasgari683@gmail.com>
2022-09-09 01:34:59 +0430

    Add Partial Views And use in layout and profile layout

---
+++ ProjectWeb.Infra.Data/Migrations/20220908201824_AddFavoriteTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectWeb.Infra.Data.Migrations
+{
+    public partial class AddFavoriteTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Favorites",
+                columns: table => new
+                {
+                    FavoriteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WebProductId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorites", x => x.FavoriteId);
+                    table.ForeignKey(
+                        name: "FK_Favorites_WebProducts_WebProductId",
+                        column: x => x.WebProductId,
+                        principalTable: "WebProducts",
+                        principalColumn: "WebProductID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favorites_WebProductId",
+                table: "Favorites",
+                column: "WebProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favorites");
+        }
+    }
+}
