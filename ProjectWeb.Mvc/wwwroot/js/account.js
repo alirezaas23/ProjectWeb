6a0d31a9388e207ec7b69e6fd9dc00653f001f91
Alireza Asgari <alirezaasgari683@gmail.com>
2022-09-29 23:27:58 +0330

    Edit JavaScripts for inputs in login and register view

--- ProjectWeb.Mvc/wwwroot/js/account.js
+++ ProjectWeb.Mvc/wwwroot/js/account.js
@@ -4,18 +4,18 @@ loginUserInput.addEventListener("focusin", function () {
     let icon = document.getElementById('login-userName-icon');
     icon.style.color = "#54BAB9";
     loginUserInput.style.outline = "none";
-    loginUserInput.style.borderBottom = "2px solid #54BAB9";
+    loginUserInput.style.border = "2px solid #54BAB9";
 });
 
 loginUserInput.addEventListener("focusout", function () {
     let icon = document.getElementById('login-userName-icon');
     if (loginUserInput.value == "") {
         icon.style.color = "red";
-        loginUserInput.style.borderBottom = "2px solid red";
+        loginUserInput.style.border = "2px solid red";
     }
     else {
         icon.style.color = "#7e8388";
-        loginUserInput.style.borderBottom = "1px solid #d1d4d7";
+        loginUserInput.style.border = "1px solid #d1d4d7";
     }
 });
 
@@ -25,18 +25,18 @@ loginPassInput.addEventListener("focusin", function () {
     let icon = document.getElementById('login-pass-icon');
     icon.style.color = "#54BAB9";
     loginPassInput.style.outline = "none";
-    loginPassInput.style.borderBottom = "2px solid #54BAB9";
+    loginPassInput.style.border = "2px solid #54BAB9";
 });
 
 loginPassInput.addEventListener("focusout", function () {
     let icon = document.getElementById('login-pass-icon');
     if (loginPassInput.value == "") {
         icon.style.color = "red";
-        loginPassInput.style.borderBottom = "2px solid red";
+        loginPassInput.style.border = "2px solid red";
     }
     else {
         icon.style.color = "#7e8388";
-        loginPassInput.style.borderBottom = "1px solid #d1d4d7";
+        loginPassInput.style.border = "1px solid #d1d4d7";
     }
 });
 
@@ -44,3 +44,4 @@ var showPass = document.getElementById('show-pass-icon');
 showPass.addEventListener("mousedown", function () {
     loginPassInput.type = "text";
 });
+
